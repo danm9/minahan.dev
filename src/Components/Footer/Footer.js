c5b9cfe23644c8fe99fe26cfa675c0d6717245ad
dan <dan@localhost.localdomain>
2022-02-13 13:01:35 -0500

    tryn somthang

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
-import './Footer.css';
+import "./Footer.css";
 
 export function Footer() {
-
   return (
-        <div className="d-flex align-items-center justify-content-center" id="footer-container">
-            <p className="text-white">
-                <strong className="text-white">M.D</strong> This website was built for an online Resume and Platform to host my Portfolio
-            </p>
-        </div>
+    <div
+      className="d-flex align-items-center justify-content-center"
+      id="footer-container"
+    >
+      <p className="text-white">
+        <strong className="text-white">M.D</strong> This website was built for
+        an online Resume and Platform to host my Portfolio
+      </p>
+    </div>
   );
 }
